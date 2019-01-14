4830a1e54a5499463124d7e0af6b0e397dfe12e4
Tommy Marshall <tmarshall@medallia.com>
2019-01-14 00:00:20 -0500

    better async

--- src/App.js
+++ src/App.js
@@ -21,26 +21,30 @@ const useTodos = (initialState) => {
 
 const App = () => {
   const [input, setInput] = useState('Add new todo!')
+  const [isLoading, setLoading] = useState(true)
   const [todos, { add, remove }] = useTodos([])
 
   const getTodos = async (add) => {
-    const res = await fetch(
-      `https://jsonplaceholder.typicode.com/todos`
-    )
-    const json = await res.json()
-    add(json.slice(0, 8).map(({ title }) => title));
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos`)
+      const json = await res.json()
+      add(json.slice(0, 8).map(({ title }) => title));
+      setLoading(false)
   };
 
-  useEffect(() => { getTodos(add) }, []);
+  useEffect(() => {
+    getTodos(add)
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <ul>
-          {todos.map((todo, i) => (
-            <li key={todo}>{todo} <button onClick={() => remove(i)}>X</button></li>
-          ))}
-        </ul>
+        {isLoading ? <p>Loading...</p> : (
+          <ul>
+            {todos.map((todo, i) => (
+              <li key={todo}>{todo} <button onClick={() => remove(i)}>X</button></li>
+            ))}
+          </ul>
+        )}
         <form onSubmit={(e) => e.preventDefault()}>
           <input value={input} onChange={(e) => setInput(e.target.value)}/>
           <button onClick={() => {
