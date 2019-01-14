0bd3152aec321df8c49a2bfb438b86f4cc01bb60
Tommy Marshall <tmarshall@medallia.com>
2019-01-13 22:50:03 -0500

    adds todos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const useTodos = (initialState) => {
+  const [todos, setTodos] = useState(initialState);
+
+  const add = (text) => {
+    setTodos([...todos, text]);
   }
+
+  const remove = (index) => {
+    todos.splice(index, 1)
+    setTodos(todos);
+  }
+
+  return [
+    todos,
+    { add, remove }
+  ]
+};
+
+const App = () => {
+  const [input, setInput] = useState('Add new todo!')
+  const [todos, { add, remove }] = useTodos(['Wash the car'])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul>
+          {todos.map((todo, i) => (
+            <li>{todo} <button onClick={() => remove(i)}>X</button></li>
+          ))}
+        </ul>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input value={input} onChange={(e) => setInput(e.target.value)}/>
+          <button onClick={() => {
+            add(input)
+            setInput('')
+          }}>Add</button>
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default App;
