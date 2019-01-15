f75191e81d9c3052df93dccc64e308b24be8a007
Tommy Marshall <tmarshall@medallia.com>
2019-01-15 14:32:25 -0500

    no custom hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-const useTodos = (initialState) => {
-  const [todos, setTodos] = useState(initialState);
+const App = () => {
+  const [input, setInput] = useState('')
+  const [todos, setTodos] = useState([])
 
   const add = (text) => {
-    if (!text) return;
-    setTodos([...todos, ...Array.isArray(text) ? [ ...text ] : [ text ]]);
+    if (!text) return
+    setTodos([...todos, ...Array.isArray(text) ? [ ...text ] : [ text ]])
   }
 
   const remove = (index) => {
     todos.splice(index, 1)
-    setTodos(todos);
+    setTodos(todos)
   }
-
-  return [
-    todos,
-    { add, remove }
-  ]
-};
-
-const App = () => {
-  const [input, setInput] = useState('')
-  const [todos, { add, remove }] = useTodos([])
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/todos`)
       .then((res) => res.json())
       .then((json) => json.slice(0, 8).map(({ title }) => title))
-      .then((t) => add(t));
+      .then((t) => add(t))
   }, [])
 
   return (
@@ -40,16 +31,16 @@ const App = () => {
       </ul>
       <form onSubmit={(e) => {
         e.preventDefault()
-        if (!input) return;
+        if (!input) return
 
-        add(input);
-        setInput('');
-      }>
+        add(input)
+        setInput('')
+      }}>
         <input placeholder="Add new todo..." value={input} onChange={(e) => setInput(e.target.value)}/>
         <button>Add</button>
       </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
