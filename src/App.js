275bd2816e05a1fbe9192cf93cf0b4474893673b
Tommy Marshall <tmarshall@medallia.com>
2019-01-15 11:45:27 -0500

    remove semicolons

--- src/App.js
+++ src/App.js
@@ -1,64 +1,64 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react'
+import './App.css'
 
 const useTodos = (initialState) => {
-  const [todos, setTodos] = useState(initialState);
+  const [todos, setTodos] = useState(initialState)
 
   const add = (text) => {
-    setTodos([...todos, ...Array.isArray(text) ? [ ...text ] : [ text ]]);
+    setTodos([...todos, ...Array.isArray(text) ? [ ...text ] : [ text ]])
   }
 
   const remove = (index) => {
     todos.splice(index, 1)
-    setTodos(todos);
+    setTodos(todos)
   }
 
   return [
     todos,
     { add, remove }
   ]
-};
+}
 
-const defaultOpts = {};
+const defaultOpts = {}
 const useFetch = (input, opts = defaultOpts) => {
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
   const {
     readBody = body => body.json(),
     ...init
-  } = opts;
+  } = opts
 
   const request = async () => {
-    setLoading(true);
+    setLoading(true)
     try {
-      const response = await fetch(input, init);
+      const response = await fetch(input, init)
       if (response.ok) {
-        const body = await readBody(response);
-        setData(body);
+        const body = await readBody(response)
+        setData(body)
       } else {
-        setError(new Error("Bad URL probs"));
+        setError(new Error("Bad URL probs"))
       }
     } catch (e) {
-      setError(e);
+      setError(e)
     }
-    setLoading(false);
-  };
+    setLoading(false)
+  }
 
   useEffect(() => {
-    request();
-  }, [input, opts]);
-  return { error, loading, data };
-};
+    request()
+  }, [input, opts])
+  return { error, loading, data }
+}
 
 const useModuleTimer = () => {
   // window.__START_TIME__
-  const start = useRef(new Date());
+  const start = useRef(new Date())
   const [finished, setFinished] = useState()
 
   const markCompleted = () => {
-    setFinished(new Date());
-  };
+    setFinished(new Date())
+  }
 
   return [
     finished ? finished - start.current : false,
@@ -69,14 +69,14 @@ const useModuleTimer = () => {
 const App = () => {
   const [input, setInput] = useState('')
   const [todos, { add, remove }] = useTodos([])
-  const {loading, data, error} = useFetch(`https://jsonplaceholder.typicode.com/todos`);
-  const [elapsedTime, markCompleted] = useModuleTimer();
+  const {loading, data, error} = useFetch(`https://jsonplaceholder.typicode.com/todos`)
+  const [elapsedTime, markCompleted] = useModuleTimer()
 
   useEffect(() => {
-    if (loading || error) return;
+    if (loading || error) return
 
-    add(data.slice(0, 8).map(({ title }) => title));
-    markCompleted();
+    add(data.slice(0, 8).map(({ title }) => title))
+    markCompleted()
   }, [loading, error])
 
   return (
@@ -90,17 +90,17 @@ const App = () => {
       )}
       <form onSubmit={(e) => {
           e.preventDefault()
-          if (!input) return;
+          if (!input) return
 
-          add(input);
-          setInput('');
+          add(input)
+          setInput('')
         }}>
         <input placeholder="Add todo..." value={input} onChange={(e) => setInput(e.target.value)}/>
         <button>Add</button>
       </form>
       {elapsedTime && <p>{elapsedTime}ms</p>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
